631fae43dd86adfbe5ebb6bfa845a0bce5a519ef
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-14 17:33:01 -0500

    Update classes with SqlAlchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models.city import City
+from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state') 
+
+    @property
+    def cities(self):
+        """Getter attribute cities that returns the list of City instances"""
+        from models import storage
+        new_list = []
+        for key, obj_city in storage.all(City).items():
+            if obj_city.state_id == self.id:
+                new_list.append(obj_city)
+        return new_list
