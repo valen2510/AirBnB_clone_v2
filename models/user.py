9cc8773dcf6ee1b63e261fe5fe31c5272f8b8db7
1uiscalderon <1857@holbertonschool.com>
2020-12-15 11:12:29 -0500

    Updated the Classes: User, Place and City

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship('Place', backref='user')
