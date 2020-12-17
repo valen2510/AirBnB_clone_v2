1c90ebe5e46d64a184a8565681f2beae6af24cec
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-16 21:17:38 -0500

    Update all unittest and add test for DBstorage and console.

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" """
+"""unitest for testing City class """
+import models
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+from models.state import State
+from sqlalchemy.exc import OperationalError
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Un"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -13,12 +16,9 @@ class test_City(test_basemodel):
         self.name = "City"
         self.value = City
 
-    def test_state_id(self):
+    def test_creation(self):
         """ """
-        new = self.value()
+        state = State(name="California")
+        new = self.value(state_id=state.id, name="San_Francisco")
         self.assertEqual(type(new.state_id), str)
-
-    def test_name(self):
-        """ """
-        new = self.value()
         self.assertEqual(type(new.name), str)
