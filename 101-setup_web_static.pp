1f8efa739680918ef10f8140f0019430ea8a7ee4
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-23 03:31:18 +0000

    | 01 | Set up server for deployment of static content with puppet. |

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,13 @@
+# Set up server for deployment of static content
+exec {'Setup_for_deployment':
+    command  => 'sudo apt-get update; 
+    sudo apt-get -y install nginx; 
+    sudo mkdir -p /data/web_static/releases/test/;
+    sudo mkdir -p /data/web_static/shared/;
+    sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html;
+    sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
+    sudo chown -R ubuntu:ubuntu /data/;
+    sudo sed -i '57 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default;
+    sudo service nginx restart',
+    provider => 'shell',
+}
