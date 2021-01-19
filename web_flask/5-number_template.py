8d0d1f2e1d2627ef556bc2d9e6aa78f3d15cff31
valen2510 <valenjaramillo1025@hotmail.com>
2021-01-19 12:35:27 -0500

    | 01 | Start a simple web application with designed template. |

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Module to start a web applicaction and
+    display binded messages
+"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function to return a greeting"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function to return a HBNB at /hbnb url bind"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_message(text):
+    """Function to return a message with c"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_message(text):
+    """Function to return a message with python"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Function to return a number with message"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Function to return display a template"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
